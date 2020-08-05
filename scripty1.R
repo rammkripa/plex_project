e0765d7054bb769d830dace66603d677056fabf1
Ram Kripa <ramkripa@Rams-Air.lan>
2020-08-05 19:10:54 +0100

    more testing of hoaxy

--- scripty1.R
+++ scripty1.R
@@ -12,12 +12,18 @@ library(hoaxy)
 # How to access hoaxy API
 articles <- hx_articles(q = "Trump",
             sort_by = "relevant")
-articles %>%
+new_df <- articles %>%
   group_by(id) %>%
-  summarize(count = n())
-tweets <- hx_tweets(articles$id[1:5])
+  summarize(count = n()) %>%
+  arrange(count) %>%
+  head(n = 15L)
 library(tidyverse)
+tweets <- hx_tweets(new_df$id)
+hx_spreaders()
 tweets %>%
   group_by(id) %>%
   mutate(tweet_id = as.numeric(tweet_id)) %>%
   summarize(tweet_id = max(tweet_id))
+library(rtweet)
+
+
